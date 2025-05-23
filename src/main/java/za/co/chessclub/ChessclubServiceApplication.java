18db2d16f01fb884ace57c91bc05f6a8cc404dff
Tebogo <Nicolasrasekgala@gmail.com>
2025-05-23 10:49:44 +0200

    first commit

---
+++ src/main/java/za/co/chessclub/ChessclubServiceApplication.java
@@ -0,0 +1,13 @@
+package za.co.chessclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessclubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessclubServiceApplication.class, args);
+	}
+
+}
